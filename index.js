b16c7a962e9e9beeb4b62b7399f674d09c07801f
Joy <deer7127@gmail.com>
2024-09-20 11:51:28 +0800

    test

---
+++ index.js
@@ -0,0 +1,6 @@
+const uniq = require("uniq");
+const ary = [1, 3, 5, 7, 7, 5, 9, 1, 1, 1, 3, 9, 5];
+const result = uniq(ary);
+console.log(result);
+console.log(process.argv[2]);
+console.log("QQQ");
